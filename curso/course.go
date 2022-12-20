8bf227ee069b6c5ca3c1dadc0d05da6ea6e2c128
Elvin Nájera <elvin7n@gmail.com>
2022-12-20 11:42:13 -0600

    commit

---
+++ curso/course.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/enajera/encapsulacion/course"
+)
+
+type Course struct {
+	Name    string
+	Price   float64
+	IsFree  bool
+	UserIDs []uint
+	Classes map[uint]string
+}
+
+func (c *Course) ChangePrice(price float64) {
+	c.Price = price
+}
+
+func (c *Course) PrintClasses() {
+	text := "Las clases son:"
+	for _, class := range c.Classes {
+		text += class + ", "
+
+	}
+
+	fmt.Println(text[:len(text)-2])
+}
